a6d16ae3a47127e7f34aa9bbd6af3f50b0915c0f
Leo Wang <q349wang@uwaterloo.ca>
2023-07-12 01:22:45 -0700

    Add gtest integration to CMake for VSCode and CI

--- base/test/test_runner.cpp
+++ base/test/test_runner.cpp
@@ -1,1 +1,10 @@
 // Copyright Antioch. All rights reserved.
+
+#include <gtest/gtest.h>
+
+TEST(Trival, Trival) { ASSERT_EQ(true, true); }
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
