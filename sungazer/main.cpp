345e8ae15c422f61eef3aa8c3aeac9ed45bf95f0
Leo Wang <q349wang@uwaterloo.ca>
2023-09-12 22:51:34 -0700

    update sim

--- sungazer/main.cpp
+++ sungazer/main.cpp
@@ -25,15 +25,15 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
   CHECK_ERROR(instance.createDevice(deviceInfo, nullptr, &device));
 
   RenderTargetCreateInfo renderTargetInfo{};
-  renderTargetInfo.extents = {.x = SCREEN_WIDTH, .y = SCREEN_HEIGHT};
+  renderTargetInfo.extents = {.x = 128, .y = 32};
   renderTargetInfo.numChannels = 3;
+  renderTargetInfo.edgeBehaviour = EdgeBehaviour::eWrap;
   RenderTarget renderTarget;
   CHECK_ERROR(device.createRenderTarget(renderTargetInfo, nullptr, &renderTarget));
 
   CommandBufferCreateInfo cmdBufInfo{};
-  CommandBuffer cmdBuf[2];
-  CHECK_ERROR(device.createCommandBuffer(cmdBufInfo, nullptr, &cmdBuf[0]));
-  CHECK_ERROR(device.createCommandBuffer(cmdBufInfo, nullptr, &cmdBuf[1]));
+  CommandBuffer cmdBuf;
+  CHECK_ERROR(device.createCommandBuffer(cmdBufInfo, nullptr, &cmdBuf));
 
   MemoryAllocateInfo allocInfo{};
   allocInfo.allocationSize = sizeof(default_glyphs::kFonts);
@@ -68,42 +68,23 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
   }
 
   BrushCreateInfo brushInfo{};
-  brushInfo.brushColour = {.r = 0x00, .g = 0xFF, .b = 0x00};
+  brushInfo.brushColour = {.r = 0xFF, .g = 0xFF, .b = 0xFF};
   Brush brush;
   CHECK_ERROR(device.createBrush(brushInfo, nullptr, &brush));
 
   CommandBufferBeginInfo beginInfo{};
-  beginInfo.clearColour.r = 0x80;
-  beginInfo.clearColour.g = 0x80;
-  beginInfo.clearColour.b = 0x80;
-  CHECK_ERROR(cmdBuf[0].reset());
-  CHECK_ERROR(cmdBuf[0].begin(beginInfo));
-  CHECK_ERROR(cmdBuf[0].bindBrush(brush));
-  CHECK_ERROR(cmdBuf[0].bindGlyph(255, glyphs));
+  beginInfo.clearColour.r = 0x00;
+  beginInfo.clearColour.g = 0x00;
+  beginInfo.clearColour.b = 0x00;
 
   std::string text1 = "Hello...";
   std::string text2 = "world?";
   std::string text3 = "@antioch";
 
-  uint32_t x = 0;
+  int32_t x = 128;
 
-  CHECK_ERROR(cmdBuf[0].drawText(text1.c_str(), text1.size(), {.x = 64 - x, .y = 1}));
-  CHECK_ERROR(cmdBuf[0].drawText(text2.c_str(), text2.size(), {.x = 64 - x, .y = 9}));
-  CHECK_ERROR(cmdBuf[0].drawText(text3.c_str(), text3.size(), {.x = 64 - x, .y = 17}));
-
-  CHECK_ERROR(cmdBuf[0].end());
-
-  std::cout << "End command buffer recording" << std::endl;
-
-  SubmitInfo submitInfo[2] = {
-      SubmitInfo{
-          .renderTarget = renderTarget, .commandBufferCount = 1, .pCommandBuffers = &cmdBuf[0]},
-      SubmitInfo{
-          .renderTarget = renderTarget, .commandBufferCount = 1, .pCommandBuffers = &cmdBuf[1]}};
-
-  CHECK_ERROR(device.submit(1, submitInfo));
-
-  std::cout << "End submission" << std::endl;
+  SubmitInfo submitInfo = {
+      .renderTarget = renderTarget, .commandBufferCount = 1, .pCommandBuffers = &cmdBuf};
 
   bool quit = false;
 
@@ -112,20 +93,20 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
   SDL_Event e;
   while (!quit) {
     const auto thisFrame = std::chrono::high_resolution_clock::now();
-    if (thisFrame - lastFrame > 250ms) {
-      x = (x + 1) % 128;
-      CHECK_ERROR(cmdBuf[0].reset());
-      CHECK_ERROR(cmdBuf[0].begin(beginInfo));
-      CHECK_ERROR(cmdBuf[0].bindBrush(brush));
-      CHECK_ERROR(cmdBuf[0].bindGlyph(255, glyphs));
+    if (thisFrame - lastFrame > 100ms) {
+      x = (x + 1) % 256;
+      CHECK_ERROR(cmdBuf.reset());
+      CHECK_ERROR(cmdBuf.begin(beginInfo));
+      CHECK_ERROR(cmdBuf.bindBrush(brush));
+      CHECK_ERROR(cmdBuf.bindGlyph(255, glyphs));
 
-      CHECK_ERROR(cmdBuf[0].drawText(text1.c_str(), text1.size(), {.x = 64 - x, .y = 1}));
-      CHECK_ERROR(cmdBuf[0].drawText(text2.c_str(), text2.size(), {.x = 64 - x, .y = 9}));
-      CHECK_ERROR(cmdBuf[0].drawText(text3.c_str(), text3.size(), {.x = 64 - x, .y = 17}));
+      CHECK_ERROR(cmdBuf.drawText(text1.c_str(), text1.size(), {.x = 128 - x, .y = 1}));
+      CHECK_ERROR(cmdBuf.drawText(text2.c_str(), text2.size(), {.x = 128 - x, .y = 9}));
+      CHECK_ERROR(cmdBuf.drawText(text3.c_str(), text3.size(), {.x = 128 - x, .y = 17}));
 
-      CHECK_ERROR(cmdBuf[0].end());
+      CHECK_ERROR(cmdBuf.end());
 
-      CHECK_ERROR(device.submit(1, submitInfo));
+      CHECK_ERROR(device.submit(1, &submitInfo));
 
       lastFrame = thisFrame;
     }
@@ -148,10 +129,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
 
   CHECK_ERROR(device.freeMemory(memory));
 
-  for (uint32_t i = 0; i < 2; i++) {
-    CHECK_ERROR(cmdBuf[i].destory());
-  }
-
+  CHECK_ERROR(cmdBuf.destory());
   CHECK_ERROR(device.destroy());
   CHECK_ERROR(instance.destroy());
   return 0;
