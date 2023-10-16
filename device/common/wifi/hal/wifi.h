b46b50546d85e1ac1d556bea788ef5b3623b532e
Victor Zhang <xskt.victor@gmail.com>
2023-10-15 17:47:55 -0700

    [esp-wifi] fill out wifi connection code

--- device/common/wifi/hal/wifi.h
+++ device/common/wifi/hal/wifi.h
@@ -11,6 +11,25 @@ namespace wifi {
 enum wifi_err_t : uint16_t {
   RES_NOOP = 1000,
   RES_OK = 0,
+
+  // esp wifi init
+  RES_NO_MEM = 1,
+  RES_INIT_OTHER = 2,
+  RES_SET_STA = 3,
+
+  RES_NOT_INIT = 4,
+
+  // esp wifi set_config
+  RES_NVS = 5,
+  RES_PSK = 6,
+  RES_SET_CONFIG_OTHER = 7,
+
+  // esp wifi start
+  RES_START = 8,
+
+  // esp wifi connect
+  RES_WIFI_SSID = 9, // "SSID of AP which station connects is invalid"
+  RES_CONN = 10,
 };
 
 [[nodiscard]] wifi_err_t wifi_hal_init();
--- device/esp32/wifi/wifi.cpp
+++ device/esp32/wifi/wifi.cpp
@@ -2,22 +2,101 @@
 
 #include "hal/wifi.h"
 
+#include <esp_wifi.h>
+
 namespace hal::wifi {
 
-wifi_err_t wifi_hal_init() { return RES_OK; }
+static wifi_config_t sta_config {
+    .sta = {
+      .ssid = "dummyssid",
+      .password = "dummypassword",
+      .bssid_set = 0,
+    },
+};
 
-wifi_err_t wifi_hal_destruct() { return RES_OK; }
+wifi_err_t wifi_hal_init() {
+  auto res = esp_event_loop_init(wifi_event_handler, nullptr);
+  
+  auto config = WIFI_INIT_CONFIG_DEFAULT();
+  res = esp_wifi_init(&config);
+  if (res == ESP_ERR_NO_MEM) {
+    return RES_NO_MEM;
+  }
+  if (res != ESP_OK) {
+    return RES_INIT_OTHER;
+  }
+  // TODO esp wifi set storage ?
+  res = esp_wifi_set_mode(WIFI_MODE_STA);
+  if (res != ESP_OK) {
+    return RES_SET_STA;
+  }
+  return RES_OK;
+}
 
-wifi_err_t wifi_set_ssid(std::string, std::string) { return RES_OK; }
+wifi_err_t wifi_hal_destruct() {
+  esp_wifi_deinit();
+  return RES_OK;
+}
 
-wifi_err_t wifi_start_connectivity() { return RES_OK; }
+wifi_err_t wifi_set_ssid(std::string ssid, std::string psk) {
+  sta_config.sta.ssid = ssid;
+  sta_config.sta.password = psk;
+  auto res = esp_wifi_set_config(WIFI_IF_STA, &sta_config);
+  if (res == ESP_ERR_WIFI_NOT_INIT) {
+    return RES_NOT_INIT;
+  }
+  if (res == ESP_ERR_WIFI_NVS) {
+    return RES_NVS;
+  }
+  if (res == ESP_ERR_WIFI_PASSWORD) {
+    return RES_PSK;
+  }
+  if (res != ESP_OK) {
+    return RES_SET_CONFIG_OTHER;
+  }
+  return RES_OK;
+}
 
-wifi_err_t wifi_stop_connectivity() { return RES_OK; }
+wifi_err_t wifi_start_connectivity() {
+  auto res = esp_wifi_start();
+  if (res == ESP_ERR_WIFI_NOT_INIT) {
+    return RES_NOT_INIT;
+  }
+  if (res == ESP_ERR_NO_MEM) {
+    return RES_NO_MEM;
+  }
+  if (res != ESP_OK) {
+    return RES_START;
+  }
+  return RES_OK;
+}
 
-wifi_err_t wifi_connect() { return RES_OK; }
+wifi_err_t wifi_stop_connectivity() {
+  esp_wifi_stop();
+  return RES_OK;
+}
 
-wifi_err_t wifi_disconnect() { return RES_OK; }
+wifi_err_t wifi_connect() {
+  auto res = esp_wifi_connect();
+  if (res == ESP_ERR_WIFI_NOT_INIT) {
+    return RES_NOT_INIT;
+  }
+  if (res == ESP_ERR_WIFI_SSID) {
+    return RES_WIFI_SSID;
+  }
+  if (res != RES_OK) {
+    return RES_CONN;
+  }
+  return RES_OK;
+}
 
-bool wifi_is_connected() { return true; }
+wifi_err_t wifi_disconnect() {
+  esp_wifi_disconnect();
+  return RES_OK;
+}
+
+bool wifi_is_connected() {
+  return true;
+}
 
 }  // namespace hal::wifi
