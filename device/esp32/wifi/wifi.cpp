5082d71b23d2eeccc3ef822d1225c2239a1fc1c9
Victor Zhang <xskt.victor@gmail.com>
2023-10-15 18:20:11 -0700

    [esp-wifi] callback handler for wifi + IP events

--- device/esp32/wifi/wifi.cpp
+++ device/esp32/wifi/wifi.cpp
@@ -4,19 +4,50 @@
 
 #include <esp_wifi.h>
 
+/* heavily inspired by
+ * https://github.com/espressif/esp-idf/blob/v5.1.1/examples/wifi/getting_started/station/main/station_example_main.c
+ */
+
 namespace hal::wifi {
 
-static wifi_config_t sta_config {
-    .sta = {
-      .ssid = "dummyssid",
-      .password = "dummypassword",
-      .bssid_set = 0,
-    },
+static constexpr char TAG[] = "[esp-wifi-hal]";
+static wifi_config_t _sta_config{
+    .sta =
+        {
+            .ssid = "dummyssid",
+            .password = "dummypassword",
+            .bssid_set = 0,
+        },
 };
+static bool _wifi_connected = false;
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id,
+                               void* event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    ESP_LOGI(TAG, "WIFI_EVENT_STA_START");
+    return;
+  }
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
+    _wifi_connected = false;
+    return;
+  }
+  if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+    ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+    _wifi_connected = true;  // wifi without IP is useless, so semantically we are only
+                             // "connected" if we get here
+    return;
+  }
+  ESP_LOGI(TAG, "wifi event %d:%d", event_base, event_id);
+}
 
 wifi_err_t wifi_hal_init() {
-  auto res = esp_event_loop_init(wifi_event_handler, nullptr);
-  
+  auto res = esp_netif_init();
+  res = esp_event_loop_create_default();
+  // todo error checking
+  esp_netif_create_default_wifi_sta();
+
   auto config = WIFI_INIT_CONFIG_DEFAULT();
   res = esp_wifi_init(&config);
   if (res == ESP_ERR_NO_MEM) {
@@ -25,23 +56,33 @@ wifi_err_t wifi_hal_init() {
   if (res != ESP_OK) {
     return RES_INIT_OTHER;
   }
-  // TODO esp wifi set storage ?
+
+  // todo error checking
+  res = esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler,
+                                            nullptr, nullptr);
+  res = esp_event_handler_instance_register(IP_EVENT, IP_EVENT_GOT_IP, &wifi_event_handler, nullptr,
+                                            nullptr);
+
   res = esp_wifi_set_mode(WIFI_MODE_STA);
   if (res != ESP_OK) {
     return RES_SET_STA;
   }
+  ESP_LOGI(TAG, "done init");
   return RES_OK;
 }
 
 wifi_err_t wifi_hal_destruct() {
   esp_wifi_deinit();
+  _wifi_connected = false;
+  _sta_config.sta.ssid = "dummyssid";
+  _sta_config.sta.password = "dummypassword";
   return RES_OK;
 }
 
 wifi_err_t wifi_set_ssid(std::string ssid, std::string psk) {
-  sta_config.sta.ssid = ssid;
-  sta_config.sta.password = psk;
-  auto res = esp_wifi_set_config(WIFI_IF_STA, &sta_config);
+  _sta_config.sta.ssid = ssid;
+  _sta_config.sta.password = psk;
+  auto res = esp_wifi_set_config(WIFI_IF_STA, &_sta_config);
   if (res == ESP_ERR_WIFI_NOT_INIT) {
     return RES_NOT_INIT;
   }
@@ -68,6 +109,7 @@ wifi_err_t wifi_start_connectivity() {
   if (res != ESP_OK) {
     return RES_START;
   }
+  ESP_LOGI(TAG, "done start_connectivity");
   return RES_OK;
 }
 
@@ -87,16 +129,16 @@ wifi_err_t wifi_connect() {
   if (res != RES_OK) {
     return RES_CONN;
   }
+  ESP_LOGI(TAG, "done connect");
   return RES_OK;
 }
 
 wifi_err_t wifi_disconnect() {
   esp_wifi_disconnect();
+  ESP_LOGI(TAG, "done disconnect");
   return RES_OK;
 }
 
-bool wifi_is_connected() {
-  return true;
-}
+bool wifi_is_connected() { return _wifi_connected; }
 
 }  // namespace hal::wifi
