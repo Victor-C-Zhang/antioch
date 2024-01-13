799487566dce3be76f0d83443f306fe61807e16b
Leo Wang <q349wang@uwaterloo.ca>
2024-01-12 21:38:04 -0800

    add esp32 input base

---
+++ device/esp32/input/input.cpp
@@ -0,0 +1,11 @@
+// Copyright Antioch. All rights reserved.
+
+#include "hal/input.h"
+
+namespace hal::input {
+
+input_err_t input_init(input_callbacks* callbacks) { return RES_OK; }
+
+input_err_t input_destruct() { return RES_OK; }
+
+}  // namespace hal::input
